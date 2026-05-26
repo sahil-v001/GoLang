df6a7ef4b8c4f0b0fbfd5056fcd811f143ec2dff
sahil-v001 <sahilvermatokyo1@gmail.com>
2026-05-26 23:29:40 +0530

    Add Go control-flow examples
    Add a new practive_go/if_else/if_else.go containing commented example snippets demonstrating Go control flow: if-else, switch (including expression-less switch), for loops (including infinite loop), and range iteration. Intended as learning/practice material.

---
+++ practive_go/if_else/if_else.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	// x := 23
+  // //                                         -------- if else -------
+	// if x > 4 {
+	// 	fmt.Println("hi bro")
+	// }else{
+	// 	fmt.Println("bye bro")
+	// }
+
+	//                                          -------- switch --------
+	// day :=3
+
+	// switch day{
+	// case 1,2,3: fmt.Println("1")
+	// case 4,5,6: fmt.Println("2")
+	// default: fmt.Println("3")
+	// }
+
+	// switch{
+	// 	case day>3: fmt.Println("1")
+	// 	case day<3: fmt.Println("2")
+	// default: fmt.Println("3")
+	// 	}
+
+	//                                      -------- loop --------
+
+	// for i:=0 ; i<4 ; i++{
+	// 	fmt.Println(i)
+	// }
+
+	//counter:= 0
+
+	// for {
+	// 	fmt.Println(counter)
+	// 	counter++
+	// }
+
+	//                                 ------- range loop ----------
+	// num := []int{0,1,2,3,4}
+
+	// for index , value:= range num{
+	// 	fmt.Println(index,value)
+	// }
+
+
+}

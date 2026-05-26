83ddf8a5fa77194d4b88c2477550eceba317423d
sahil-v001 <sahilvermatokyo1@gmail.com>
2026-05-26 14:25:34 +0530

    Add variable demos and prints in main
    Replace the previous Hello World example and external my_utils call with local variable demonstrations. Adds several variables (string, int, float, bool, const), prints them, reassigns an int, and declares a short variable (SAHIL). Also removes the unused my_utils import.

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"MyLearning/my_utils"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
-	my_utils.Printmsg("hahahaha")
+	var name = "hi sahil"
+	var number = 54
+	var number1 = 25.123
+	var number2  = "C"
+	var boy = true
+	const game = 34
+
+	fmt.Println(number)
+	fmt.Println(name)
+	fmt.Println(number1)
+	fmt.Println(number2)
+	fmt.Println(boy)
+	fmt.Println(game)
+
+	number = 98
+
+	SAHIL :=67
+	fmt.Println(SAHIL)
 }
